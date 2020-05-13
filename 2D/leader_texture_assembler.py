6bca764c1a7a5b4a6f6e2512dd0cacce1b4d2491
deliverator23 <iain.millar@gmail.com>
2020-05-13 04:53:18 +0100

    Update Leader texture scripts.

--- 2D/leader_texture_assembler.py
+++ 2D/leader_texture_assembler.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 import os
 from pathlib import Path
-
+import ntpath
 
 def get_concat_h(im1, im2):
     dst = Image.new('RGB', (im1.width + im2.width, im1.height))
@@ -17,9 +17,10 @@ def get_concat_v(im1, im2):
     dst.paste(im2, (0, im1.height))
     return dst
 
-texture_filename = "gajah_accessories_diff.dds"
+texture_filename = "gajah_hair_diff.dds"
 
 dir_path = "C:\\Users\\User\\Documents\\My Games\\Sid Meier's Civilization 5\\ScreenShots"
+temp_path = "D:\\temp"
 
 paths = sorted(Path(dir_path).iterdir(), key=os.path.getmtime)
 
@@ -32,16 +33,16 @@ images = []
 for image_path in last_four_screenshots:
     image = Image.open(image_path).convert("RGB")
     image = image.crop((445, 21, 1469, 1045))
-    png_filename = str(image_path).replace(".tga", ".png")
+    png_filename = temp_path + "\\" + str(ntpath.basename(image_path)).replace(".tga", ".png")
     image.save(png_filename)
-    print(image.size)
+    print(image_path, image.size)
     images.append(image)
 
 row1 = get_concat_h(images[0], images[1])
 row2 = get_concat_h(images[2], images[3])
 
-row1.save(dir_path + "\\" + "temp_row1.png")
-row2.save(dir_path + "\\" + "temp_row2.png")
+row1.save(temp_path + "\\" + "temp_row1.png")
+row2.save(temp_path + "\\" + "temp_row2.png")
 
 full_image = get_concat_v(row1, row2)
 
---
+++ 2D/leader_texture_cropper.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+import os
+from pathlib import Path
+
+texture_filename = "gajah_hair_norm.dds"
+
+dir_path = "C:\\Users\\User\\Documents\\My Games\\Sid Meier's Civilization 5\\ScreenShots"
+
+paths = sorted(Path(dir_path).iterdir(), key=os.path.getmtime)
+
+image_path = paths[-1:][0]
+
+image = Image.open(image_path).convert("RGB")
+image = image.crop((445, 21, 1469, 1045))
+png_filename = str(image_path).replace(".tga", ".png")
+image.save("D:\\Civ5Mod\\Leaderhead_Work\\HQ_Textures\\" + texture_filename.replace(".dds",".png"))
+print(image.size)
