e7373ad0a25584f008197f4ded365ec2836ff969
deliverator23 <iain.millar@gmail.com>
2020-05-28 15:59:11 +0100

    Add 256 size.

--- 2D/icon_resizer.py
+++ 2D/icon_resizer.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 dir_path = "D:\\Civ6Mod\\2D\\LeaderPortrait\\Resize_Bulk_Test"
 
-sizes = [32, 45, 50, 55, 64, 80] #256
+sizes = [32, 45, 50, 55, 64, 80, 256]
 
 for file in os.listdir(dir_path):
     fullpath = dir_path + "\\" + str(file)
